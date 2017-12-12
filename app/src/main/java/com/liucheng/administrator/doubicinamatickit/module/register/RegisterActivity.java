78509fc157b69485a0ccc9f9373cbbc67a10bc6d
zouliuqin <511068289@qq.com>
2017-12-12 08:59:12 +0800

    2017.12.12

--- app/src/main/java/com/liucheng/administrator/doubicinamatickit/module/register/RegisterActivity.java
+++ app/src/main/java/com/liucheng/administrator/doubicinamatickit/module/register/RegisterActivity.java
@@ -135,6 +135,7 @@ public class RegisterActivity extends AppCompatActivity {
                             bu.setPassword(password);
 
 
+
                             //注意：不能用save方法进行注册
                             bu.signUp(new SaveListener<User>() {
                                 @Override
